7d9a612dfee2a3c88954ee3271efcc0f7ee1c83b
Gri <2nazarovgrigoriy@gmail.com>
2022-02-21 15:44:56 +0200

    !!!

---
+++ src/main/java/com/nazarov/shop/web/servlets/security/SecurityAuthChecker.java
@@ -0,0 +1,24 @@
+package com.nazarov.shop.web.servlets.security;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public class SecurityAuthChecker {
+
+    public boolean checkUserAuthorization(HttpServletRequest request, List<String> userTokens) {
+        boolean isAuth = false;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("user-token")) {
+                    if (userTokens.contains(cookie.getValue())) {
+                        isAuth = true;
+                    }
+                    break;
+                }
+            }
+        }
+        return isAuth;
+    }
+}
